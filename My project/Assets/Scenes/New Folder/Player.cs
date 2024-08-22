a94d8e5e46375b54d3b2cc94536649d210bc2d4a
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2024-08-22 11:47:11 -0700

    added movement
    Almost done

--- My project/Assets/Scenes/New Folder/Player.cs
+++ My project/Assets/Scenes/New Folder/Player.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 velocity;
+
     void Start()
     {
-        Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update");
+        velocity = Vector2.zero;
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(velocity);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
     }
 }
